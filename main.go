a9a7ace6fb77fd82086d0675262e3ec7a5e1a7a4
Alexandru Cojocaru <xojoc@gmx.com>
2015-04-30 23:47:07 +0200

    s

--- main.go
+++ main.go
@@ -312,7 +312,7 @@ func likeHandler(w http.ResponseWriter, r *http.Request) {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "" || r.URL.Path == "/" || r.URL.Path == "/index.html" {
-		rows, err := DB.Query(`select JokeID,Joke,Reply,Likes from Jokes`)
+		rows, err := DB.Query(`select JokeID,Joke,Reply,Likes from Jokes limit 20;`)
 		if err != nil {
 			if err == sql.ErrNoRows {
 				http.NotFound(w, r)
