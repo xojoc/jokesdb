b7b45a10a8df4caa301df0ef98166f0dbfbfe185
Alexandru Cojocaru <xojoc@gmx.com>
2015-05-05 13:17:43 +0200

    s

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func init() {
 const (
 	SiteTitle    = "BarzeDette :)"
 	JokeString   = "Barzelletta"
-	PageTitle    = "BarzeDette :) - Barzellette"
+	IndexTitle   = "BarzeDette :) - Barzellette"
 	Domain       = "http://barzedette.pw"
 	Sha512passwd = "4c516647bf061fa36a28fbb09d54e08f0d17b915b3edc5b07b5dc550ba5f8b447143f2660e3b9d77a20479e6a3f2de5e9fa64113ea44024eb9bc9f08d217b413"
 	PathJoke     = "/barzelletta/"
@@ -80,7 +80,7 @@ type NetError struct {
 }
 
 func DefaultTitle() string {
-	return "BarzeDette :) - Barzellette"
+	return IndexTitle
 }
 
 func min(a int, b int) int {
