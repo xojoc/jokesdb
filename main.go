18bb3897b84241b1d5bad364ad027892fe5cdcf8
Alexandru Cojocaru <xojoc@gmx.com>
2015-05-05 13:17:01 +0200

    s

--- main.go
+++ main.go
@@ -29,7 +29,9 @@ func init() {
 }
 
 const (
-	PageTitle    = "Barzedette: barzellette"
+	SiteTitle    = "BarzeDette :)"
+	JokeString   = "Barzelletta"
+	PageTitle    = "BarzeDette :) - Barzellette"
 	Domain       = "http://barzedette.pw"
 	Sha512passwd = "4c516647bf061fa36a28fbb09d54e08f0d17b915b3edc5b07b5dc550ba5f8b447143f2660e3b9d77a20479e6a3f2de5e9fa64113ea44024eb9bc9f08d217b413"
 	PathJoke     = "/barzelletta/"
@@ -92,7 +94,7 @@ func (j *Joke) AbsUrl() string {
 	return Domain + PathJoke + strconv.FormatUint(j.JokeID, 10)
 }
 func (j *Joke) Title() string {
-	return "BarzeDette :) | Barzelletta: " + j.Joke[:min(15, len(j.Joke))] + "..."
+	return SiteTitle + " | " + JokeString + ": " + j.Joke[:min(15, len(j.Joke))] + "..."
 }
 
 /*
