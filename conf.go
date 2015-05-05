698f0a53fd92dc80a7a05dbd62cd3960611883ca
Alexandru Cojocaru <xojoc@gmx.com>
2015-05-05 14:03:58 +0200

    s

--- conf.go
+++ conf.go
@@ -1,13 +1,13 @@
 package main
 
 const (
-	SiteTitle    = "BarzeDette :)"
-	JokeString   = "Barzelletta"
-	IndexTitle   = "BarzeDette :) - Barzellette"
-	Domain       = "http://barzedette.pw"
+	SiteTitle    = "Jokes.pw"
+	JokeString   = "Joke"
+	IndexTitle   = "Jokes.pw"
+	Domain       = "http://jokes.pw"
 	Sha512passwd = "4c516647bf061fa36a28fbb09d54e08f0d17b915b3edc5b07b5dc550ba5f8b447143f2660e3b9d77a20479e6a3f2de5e9fa64113ea44024eb9bc9f08d217b413"
-	PathJoke     = "/barzelletta/"
-	PathCategory = "/barzellette/"
-	PathSubmit   = "/invia"
+	PathJoke     = "/joke/"
+	PathCategory = "/jokes/"
+	PathSubmit   = "/submit"
 	PathAdmin    = "/admin"
 )
