b6c7a1b349b53177cc0c861a9d164591bf04dfd9
Alexandru Cojocaru <xojoc@gmx.com>
2015-05-19 23:20:26 +0200

    s

--- main.go
+++ main.go
@@ -101,6 +101,31 @@ func (j *Joke) TweetButton() *TweetButton {
 	return t
 }
 */
+
+func (cj *Joke) Next() *Joke {
+	j := &Joke{}
+	err := DB.QueryRow(`select JokeID, Joke, Likes, Date, CategoryID from Jokes
+where JokeID > ? and CategoryID = ?
+order by JokeID asc limit 1;`, cj.JokeID, cj.Category.CategoryID).Scan(&j.JokeID, &j.Joke, &j.Likes, &j.Date, &j.CategoryID)
+	if err != nil {
+		return nil
+	}
+	j.Category = cj.Category
+	return j
+}
+
+func (cj *Joke) Prev() *Joke {
+	j := &Joke{}
+	err := DB.QueryRow(`select JokeID, Joke, Likes, Date, CategoryID from Jokes
+where JokeID < ? and CategoryID = ?
+order by JokeID desc limit 1;`, cj.JokeID, cj.Category.CategoryID).Scan(&j.JokeID, &j.Joke, &j.Likes, &j.Date, &j.CategoryID)
+	if err != nil {
+		return nil
+	}
+	j.Category = cj.Category
+	return j
+}
+
 func (j *Joke) WasLiked(r *http.Request) {
 	c, err := r.Cookie("uuid")
 	if err != nil {
